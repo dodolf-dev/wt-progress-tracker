d308804ee68a72913551917641517bf877bd53cc
dodolf-dev <grimelins1234@gmail.com>
2025-11-19 23:11:40 +0100

    Co-authored-by: ✧ <cyb3r-dev@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/Home';
+import Error404 from './pages/Error404/Error404';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="*" element={<Error404/>}/>
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export default function Error404() {
+    return (
+        <div className="error404">
+            <p className="error404_subtitle">Oups! La page que vous demandez n'existe pas.</p>
+        </div>
+    )
+}
